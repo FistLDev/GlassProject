1521b00e296ed2be399ca65e2fdff22416a4d8ee
Алексей Симоненков <simonenkov.a@s2snext.com>
2021-05-08 13:02:33 +0300

    add personal account controller and view

---
+++ controllers/PersonalAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GlassProject.controllers
+{
+    public class PersonalAccountController : Controller
+    {
+        public IActionResult PersonalAccount()
+        {
+            return View();
+        }
+    }
+}
